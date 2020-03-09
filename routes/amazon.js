d6d929d22137dcda300de896df4bd83f3c32fdfa
bankkeez <sethitikul@gmail.com>
2020-03-09 15:16:01 +0700

    add more endpoints

--- routes/amazon.js
+++ routes/amazon.js
@@ -6,8 +6,37 @@ var router = express.Router();
 
 router.get('/test', function(req, res, next) {
     s3.listBuckets(function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     res.send(data);           // successful response
+        if (err) console.log(err, err.stack);
+        else     res.send(data);
+    });
+});
+
+router.get('/image/:key',function(req,res,next){
+    var params = {
+        Bucket: "mabaguette", 
+        Key: req.params.key
+       };
+    s3.getObject(params, function(err, data) {
+        if (err) console.log(err, err.stack);
+        else     res.send(data);
+    });
+});
+
+router.get('/images',function(req,res,next){
+    var params = {
+        Bucket: "mabaguette"
+       };
+       s3.listObjects(params, function(err, data) {
+        if (err) console.log(err, err.stack);
+        else     res.send(data.Contents.map(element => element.Key));
+      });
+});
+
+router.post('/image',function(req,res,next){
+    reader.readAsDataURL();
+    s3.putObject(req.body, function(err, data) {
+        if (err) console.log(err, err.stack);
+        else     res.send(res.statusCode);
     });
 });
 
